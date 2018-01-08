3b9c5f21b6f07cbf41f5fb59e14110b3ab8d8f0b
unknown <liliana.quyen.tang@gmail.com>
2018-01-07 17:09:32 -0700

    all db login connection variables

---
+++ db_config.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define("DB_USER", "root"); //db user
+define("DB_PASSWORD", "GetGo"); //db password 
+define("DB_DATABASE", "login");
+define("DB_HOST", "localhost");
+
+?>
